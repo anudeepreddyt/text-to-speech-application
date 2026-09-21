f5425604ff7fceccb1448caff704b7bae7691250
anudeepreddyt <anudeepreddy4637@gmail.com>
2026-09-21 15:15:35 +0530

    Updated backend for CORS error

--- text-to-speech-backend/text-to-speech-backend/src/main/java/com/anudeepreddy/text_to_speech_backend/Filter/JWTFilter.java
+++ text-to-speech-backend/text-to-speech-backend/src/main/java/com/anudeepreddy/text_to_speech_backend/Filter/JWTFilter.java
@@ -31,23 +31,29 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token=null;
-        String username=null;
-        String authHeader=request.getHeader("Authorization");
+       try {
+            String token = null;
+            String username = null;
+            String authHeader = request.getHeader("Authorization");
 
-        if(authHeader!=null && authHeader.startsWith("Bearer ")){
-            token=authHeader.substring(7);
-            username=jwtService.extractUsername(token);
-        }
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = jwtService.extractUsername(token);
+            }
 
-        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            UserDetails userDetails=myUserDetailService.loadUserByUsername(username);
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = myUserDetailService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 
-        }
-        filterChain.doFilter(request,response);
+            }
+
+        } catch (Exception exception) {
+           // Invalid or expired JWT
+           SecurityContextHolder.clearContext();
+       }
+        filterChain.doFilter(request, response);
     }
 
     @Override
